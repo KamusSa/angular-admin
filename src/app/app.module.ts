c8ef1bb515e7bcfad787fbc86fa1c3201979c8cb
KamusSa <luiscaifan4@gmail.com>
2022-11-28 22:18:06 -0600

    Pantalla de Login y Register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
 
 
 @NgModule({
@@ -28,6 +29,7 @@ import { Grafica1Component } from './pages/grafica1/grafica1.component';
     HeaderComponent,
     ProgressComponent,
     Grafica1Component,
+    PagesComponent,
   ],
   imports: [
     BrowserModule,
