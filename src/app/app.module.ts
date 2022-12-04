92e4bfa917ff2b0ccf3cd6ce99fdb632425d3f4b
acluis <luis.aguilarcervantes@proveedoresgnp.mx>
2022-12-04 12:26:39 -0600

    Barras de Progreso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 // Modulos
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';  
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
 import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
 
-    NotpagefoundComponent, 
+    NotpagefoundComponent,
   ],
   imports: [
     BrowserModule,
