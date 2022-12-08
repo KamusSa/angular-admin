895577aa7521922fa49656d14e4af043d05e843a
acluis <luis.aguilarcervantes@proveedoresgnp.mx>
2022-12-07 22:22:23 -0600

    Verion ESTABLE miercoles 7 diciembre

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PagesComponent } from './pages.component';
 import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
 
 
 const routes: Routes = [
@@ -12,7 +13,7 @@ const routes: Routes = [
         children: [
             { path: '', component: DashboardComponent },
             { path: 'progress', component: ProgressComponent },
-            { path: 'grafica1', component: ProgressComponent },
+            { path: 'grafica1', component: Grafica1Component },
        
         ]
     },
